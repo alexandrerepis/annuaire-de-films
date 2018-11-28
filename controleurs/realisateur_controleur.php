92511f95b02bc72fee665b5abaf20ca763e6b1e7
Valuminous <valerie.h@codeur.online>
2018-11-28 11:50:25 +0100

    modifs des controleurs

--- controleurs/realisateur_controleur.php
+++ controleurs/realisateur_controleur.php
@@ -1,8 +1,50 @@
 <?php
 
-require 'realisateur_modeles.php'
+require_once 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('vues');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+));
 
-echo $twig->render('realisateur.twig', array ( '' => $, '' => $, '' => $,'' => $, ));
+require_once("modeles/realisateur_modeles.php");
 
+   
+    
+    function realList() {
+        global $twig;
+        $realisateurs = bdd_realList();
+        // var_dump($realisateurs);
+        $template = $twig->load('realisateur.twig');
+        echo $template->render(array('realisateurs' => $realisateurs));
+    }
 
-?>
+    function realDetail() {
+        global $twig;
+        global $id;
+        $realisateurs = bdd_realDetail();
+        
+        $template = $twig->load('realisateur.twig');
+        for ($i = 0; $i < 10; $i++) {
+            if ($id === $i) {
+                $details = bdd_realDetail($i);
+            } elseif ($id < 1 || $id > 9) {
+                $details = bdd_realDetail(1);
+            }
+        }
+        echo $template->render(array('details' => $details));
+
+    }
+    
+switch ($action) {
+    case 'list':
+        realList();
+        break;
+
+    case 'detail':
+        realDetail();
+        break;
+    
+    default:
+        realList();
+        break;
+}
